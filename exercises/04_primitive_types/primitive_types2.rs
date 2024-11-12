a2bfdb19ca46819b650a5632d274f3cf1c589024
nappingrn <dylan.r.trout@gmail.com>
2024-11-11 21:51:29 -0500

    finish primitive types

--- exercises/04_primitive_types/primitive_types2.rs
+++ exercises/04_primitive_types/primitive_types2.rs
@@ -4,6 +4,7 @@ fn main() {
     // Note the _single_ quotes, these are different from the double quotes
     // you've been seeing around.
     let my_first_initial = 'C';
+
     if my_first_initial.is_alphabetic() {
         println!("Alphabetical!");
     } else if my_first_initial.is_numeric() {
@@ -17,6 +18,8 @@ fn main() {
     // Try a letter, try a digit (in single quotes), try a special character, try a character
     // from a different language than your own, try an emoji 😉
     // let your_character = '';
+
+    let your_character : char = 'M';
 
     if your_character.is_alphabetic() {
         println!("Alphabetical!");
