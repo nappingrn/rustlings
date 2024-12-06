86e8bc5ad4592989fb55e3a1fcaf42141068a463
nappingrn <dylan.r.trout@gmail.com>
2024-12-05 21:00:27 -0500

    match statements

--- exercises/13_error_handling/errors2.rs
+++ exercises/13_error_handling/errors2.rs
@@ -23,7 +23,13 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     // TODO: Handle the error case as described above.
     let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    let toReturn = match qty {
+        Ok(T) => Ok(T * cost_per_item + processing_fee),
+        Err(T) => Err(T)
+    };
+
+    toReturn
+
 }
 
 fn main() {
